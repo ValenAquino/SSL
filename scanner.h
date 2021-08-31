1d5f8e88c20224ddc572167017e0280a504dab76
ValenAquino <70243484+ValenAquino@users.noreply.github.com>
2021-08-31 01:50:13 -0300

    TP0 v1

---
+++ scanner.h
@@ -0,0 +1,18 @@
+  
+#ifndef SCANNER_H_INCLUDED
+#define SCANNER_H_INCLUDED
+
+enum TOKEN{
+    FDT,
+    SEP,
+    CAD
+};
+
+struct scan{
+    char cadena[100];
+    int token;
+};
+
+struct scan get_token();
+
+#endif // SCANNER_H_INCLUDED
